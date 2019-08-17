c869b80f05ab4af950b27536d63960811b3d21f2
b0wter <b0wter@gmx.de>
2019-08-17 21:55:21 +0200

    Changed nuget creation script so it automatically uploads the resulting package.

--- tests/base_tests/Program.fs
+++ tests/base_tests/Program.fs
@@ -4,7 +4,4 @@ module BaseTests.Program
 let main args =
     printfn "This project only contains unit tests. Please run it via `dotnet test`."
 
-
-    let x = b0wter.FSharp.Validation.ValidationResult.Default<string>.Success "lul" 
-
     0
