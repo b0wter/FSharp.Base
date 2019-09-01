97c04d16f803282aa28abbdb301b52a5a94d9d83
b0wter <b0wter@gmx.de>
2019-09-01 19:32:03 +0200

    Added summary tags for the parser methods.

--- src/base/Parsers.fs
+++ src/base/Parsers.fs
@@ -6,11 +6,20 @@ module Parsers =
     let private returnIfSuccess (success: bool, result: 'a) : 'a option =
         if success then Some result else None
 
+    /// <summary>
+    /// Takes a string and returns Some int if the string contains an integer, otherwise None.
+    /// </summary>
     let parseInt (s: string) : int option =
         returnIfSuccess <| Int32.TryParse(s)
 
+    /// <summary>
+    /// Takes a string and returns Some double if the string contains a floating point number, otherwise None.
+    /// </summary>
     let parseFloat (s: string) : float option =
         returnIfSuccess <| Double.TryParse(s)
 
+    /// <summary>
+    /// Takes a string and returns Some Guid if the string contains a Guid, otherwise None.
+    /// </summary>
     let parseGuid (s: string) : System.Guid option =
         returnIfSuccess <| Guid.TryParse(s)
