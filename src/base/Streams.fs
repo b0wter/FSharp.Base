c062fcbf2a1b3fb4d3a853d8017df9661aa75235
b0wter <b0wter@gmx.de>
2019-09-04 12:32:37 +0200

    Renamed Stream methods to reflect what they are doing.

--- src/base/Streams.fs
+++ src/base/Streams.fs
@@ -5,13 +5,13 @@ module Streams =
     /// <summary>
     /// Reads a stream to the end synchrounously.
     /// </summary>
-    let readStream (encoding: System.Text.Encoding) (stream: System.IO.Stream) =
+    let readToEnd (encoding: System.Text.Encoding) (stream: System.IO.Stream) =
         use reader = new System.IO.StreamReader(stream, encoding)
         reader.ReadToEnd ()
 
     /// <summary>
     /// Reads a stream to the end asynchrounously.
     /// </summary>
-    let readStreamAsync (encoding: System.Text.Encoding) (stream: System.IO.Stream) =
+    let readToEndAsync (encoding: System.Text.Encoding) (stream: System.IO.Stream) =
         use reader = new System.IO.StreamReader(stream, encoding)
         reader.ReadToEndAsync () |> Async.AwaitTask
