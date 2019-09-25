4fcc578c7efd5a1ff61d604900fa636879f5eb72
b0wter <b0wter@gmx.de>
2019-09-25 11:09:43 +0200

    Added Union.isUnionCase
    Added a method that allows the user to check if a value is a certain
    case of a union type.

---
+++ src/base/Union.fs
@@ -0,0 +1,26 @@
+namespace b0wter.FSharp
+
+module Union =
+
+    open Microsoft.FSharp.Quotations.Patterns
+    open Microsoft.FSharp.Reflection
+
+    /// <summary>
+    /// Checks if a value is a certain case of a union. Use like this:
+    /// > myValue |> isUnionCase <@ MyUnionCase @>
+    /// </summary>
+    /// <remarks>
+    /// I have not written this code myself, it's from
+    /// https://stackoverflow.com/a/11798829
+    /// </remarks>
+    let rec isUnionCase = function
+        | Lambda (_, expr) | Let (_, _, expr) -> isUnionCase expr
+        | NewTuple exprs -> 
+            let iucs = List.map isUnionCase exprs
+            fun value -> List.exists ((|>) value) iucs
+        | NewUnionCase (uci, _) ->
+            let utr = FSharpValue.PreComputeUnionTagReader uci.DeclaringType
+            box >> utr >> (=) uci.Tag
+        | _ -> failwith "Expression is no union case."
+
+    
---
+++ tests/base_tests/Union.fs
@@ -0,0 +1,28 @@
+namespace BaseTests.Result
+
+module isUnionCase =
+
+    open FsUnit
+    open FsUnit.Xunit
+    open Xunit
+    open b0wter.FSharp
+
+    type TestUnion
+        = First
+        | Second
+        | Third
+
+    [<Fact>]
+    let ``Given a union type of matching case returns true`` () =
+        let value = First
+        value |> Union.isUnionCase<@ First @> |> should be True
+
+    [<Fact>]
+    let ``Given a union type of non-matching case returns f`` () =
+        let value = Second
+        value |> Union.isUnionCase<@ First @> |> should be False
+
+    [<Fact>]
+    let ``Given a non-union type throws an exception`` () =
+        let value = Third
+        (fun () -> value |> Union.isUnionCase<@ int @> |> ignore) |> should (throwWithMessage "Expression is no union case.") typeof<System.Exception>
