692c1e3a32e685f35f37d9e76d6599ac2ea97546
b0wter <b0wter@gmx.de>
2019-10-23 20:12:25 +0200

    Added bind(A)/map(A)Error to AsyncResult.

--- src/base/AsyncResult.fs
+++ src/base/AsyncResult.fs
@@ -11,6 +11,15 @@ module AsyncResult =
             | Error e -> return Error e
         }
         
+    /// Works like `Result.bindError` except that the result and the function are async.
+    let bindErrorA (asyncOp: 'b -> Async<Result<'a, 'c>>) (asyncResult: Async<Result<'a, 'b>>) : Async<Result<'a, 'c>> =
+        async {
+            let! b = asyncResult
+            match b with
+            | Ok o -> return Ok o
+            | Error e -> return! e |> asyncOp
+        }
+        
     /// Works like `Result.bind` except that the initial result is async (`Async<Result<_,_>>`)
     /// while the function is not.
     let bind syncOp asyncResult =
@@ -19,6 +28,16 @@ module AsyncResult =
             match b with
             | Ok o -> return syncOp o
             | Error e -> return Error e
+        }
+        
+    /// Works like `Result.bindError` except that the initial result is async (`Async<Result<_,_>>`)
+    /// while the function is not.
+    let bindError (syncOp: 'b -> Result<'a, 'c>) (asyncResult: Async<Result<'a, 'b>>) : Async<Result<'a, 'c>> =
+        async {
+            let! b = asyncResult
+            match b with
+            | Ok o -> return Ok o
+            | Error e -> return e |> syncOp
         }
 
     /// Works like `Result.map` except that the result and the function are async.
@@ -31,6 +50,16 @@ module AsyncResult =
             | Error e -> return Error e
         }
         
+    /// Works like `Result.mapError` except that the result and the function are async.
+    let mapErrorA (asyncOp: 'b -> Async<'c>) (asyncResult: Async<Result<'a, 'b>>) : Async<Result<'a, 'c>> =
+        async {
+            let! b = asyncResult
+            match b with
+            | Ok o -> return Ok o
+            | Error e -> let! c = (e |> asyncOp)
+                         return Error c
+        }
+        
     /// Works like `Result.map` except that the initial result is async (`Async<Result<_,_>>`)
     /// while the function is not.
     let map syncOp asyncResult =
@@ -39,4 +68,14 @@ module AsyncResult =
             match b with
             | Ok o -> return (Ok <| syncOp o)
             | Error e -> return Error e
+        }
+        
+    /// Works like `Result.mapError` except that the initial result is async (`Async<Result<_,_>>`)
+    /// while the function is not.
+    let mapError (syncOp: 'b -> 'c) (asyncResult: Async<Result<'a, 'b>>) : Async<Result<'a, 'c>> =
+        async {
+            let! b = asyncResult
+            match b with
+            | Ok o -> return Ok o
+            | Error e -> return (Error <| syncOp e)
         }
