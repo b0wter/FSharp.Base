1cabc0a7d02a1c7bc95e6acf3ac1042be6629a69
b0wter <b0wter@gmx.de>
2019-11-18 12:21:13 +0100

    Added String.join

--- src/base/String.fs
+++ src/base/String.fs
@@ -4,3 +4,6 @@ module String =
 
     /// Checks wether a string contains any chars that are non-whitespace-characters.
     let isNullOrWhiteSpace = System.String.IsNullOrWhiteSpace
+
+    /// Wraps System.String.Join to allow it to be partially applied.
+    let join (delimiter: string) (strings: string seq) = System.String.Join(delimiter, strings)
