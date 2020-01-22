44349fa38e4f569d18747c39640c01ca51e56b1e
b0wter <b0wter@gmxd.de>
2020-01-22 13:11:25 +0100

    Added tests for String.join
    The method simply calls the BCL String.Join
    so tests are not necessary. This is for the case that
    the implementation is changed in the future and nobody
    remembers that there are no tests for String.join

--- tests/base_tests/String.fs
+++ tests/base_tests/String.fs
@@ -37,3 +37,29 @@ module IsNullOrWhitespace =
     [<MemberData("whiteSpaceTestCases")>]
     let ``Given a string containing only whitespace characters, returns true.`` s =
         s |> String.isNullOrWhiteSpace |> should be True
+
+module Join =
+
+    open FsUnit
+    open FsUnit.Xunit
+    open Xunit
+    open b0wter.FSharp
+
+    type TestData = {
+        parts: string list
+        delimiter: string
+        result: string
+    }
+
+    let testCases () : obj array seq =
+        seq {
+            yield [| { parts = [ "a"; "b"; "c" ]; delimiter = ""; result = "abc" } |]
+            yield [| { parts = [ "d"; "e"; "f" ]; delimiter = ","; result = "d,e,f" } |]
+            yield [| { parts = [ "a"; ]; delimiter = ","; result = "a" } |]
+            yield [| { parts = [ ]; delimiter = ","; result = "" } |]
+        }
+
+    [<Theory>]
+    [<MemberData("testCases")>]
+    let ``Given a list of strings and a delimiter, returns a single string containing all elements.`` data =
+        (String.join data.delimiter data.parts) |> should equal data.result
